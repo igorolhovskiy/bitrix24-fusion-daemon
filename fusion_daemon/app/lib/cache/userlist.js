0af668eb9be28c6e928b06eb56b58a9fdce28ff2
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-06-15 20:52:20 +0300

    Get userlist - extension data

---
+++ fusion_daemon/app/lib/cache/userlist.js
@@ -0,0 +1,66 @@
+var cache = require('memory-cache'),
+    log = require('../../init/logger')(module),
+    request = require('urllib');
+
+function getEmployeeList(bitrixURL, callback) {
+    
+    let employeeList = cache.get('employeeList');
+
+    if (employeeList) {
+        log("Got data from cache!");
+        callback(null, employeeList);
+        return;
+    }
+
+    log("Cache is empty, getting data from server...");
+
+    let requestURL = bitrixURL + "/user.get.json?USER_TYPE=employee";
+
+    request.request(requestURL, (err, data, res) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (res.statusCode !== 200) {
+            callback(true, "Server failed to answer with " + res.statusCode + " code");
+        }
+
+        let userList = data.toString();
+
+        try {
+            userList = JSON.parse(userList);
+        } catch (e) {
+            callback(true, "Answer from server is not JSON");
+            return;
+        }
+
+        if (typeof userList.result === 'undefined') {
+            callback(true, "Missing result section in answer");
+            return;
+        }
+
+        userList = userList.result;
+
+        let result = {};
+
+        for (let user in userList) {
+
+            if (typeof userList[user].UF_PHONE_INNER === 'undefined' || userList[user].UF_PHONE_INNER === null) {
+                continue;
+            }
+
+            if (typeof userList[user].ID === 'undefined') {
+                log("Strange, we have a user without ID: " + JSON.stringify(user));
+                continue;
+            }
+            result[userList[user].UF_PHONE_INNER] = userList[user].ID;
+        }
+
+        cache.put('employeeList', result, 3600);
+
+        callback(null, result);
+    });
+}   
+
+module.exports = getEmployeeList;
