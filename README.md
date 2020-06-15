b3ec1d76e77708d207155897ace2d95361fe6f07
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-06-15 19:51:43 +0300

    Initial rework

---
+++ README.md
@@ -0,0 +1,3 @@
+### Daemon to listen FreeSWITCH Events and send em to Bitrix24 (box) via webHooks
+
+For all questions, pls fork and explore.
---
+++ fusion_daemon/app/init/logger.js
@@ -0,0 +1,12 @@
+const options = require('../config/freeswitch');
+
+module.exports = module => {
+    return function() {
+        if (options.debug) {
+            let currentDate = new Date().toISOString();
+            currentDate = currentDate.slice(0,10) + " " + currentDate.slice(11,19);
+            let args = ["[" + currentDate +  "]" ,"[" + module.filename + "]"].concat([].slice.call(arguments));
+            console.log.apply(console, args)
+        }
+    }
+}
