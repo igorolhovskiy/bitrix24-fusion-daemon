b274561fe03a5833ffeafa4216e5a12f43c8f550
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-06-18 18:09:40 +0300

    ...

--- fusion_daemon/app/index.js
+++ fusion_daemon/app/index.js
@@ -2,8 +2,11 @@ const freeswitch = require('./init/freeswitch'),
     headersProcess = require('./init/fsheadersprocess'),
     log = require('./init/logger')(module),
     callRinging = require('./lib/calls/progress'),
-    callAnswer = require('./lib/calls/bridge');
+    callAnswer = require('./lib/calls/bridge'),
+    cache = require('memory-cache');
     //callHangup = require('./lib/hangup');
+
+const request = require('urllib');
 
 freeswitch
     .on('esl::event::CHANNEL_PROGRESS::*', function(e) {
@@ -11,14 +14,14 @@ freeswitch
         if (typeof(headers['variable_bitrix24_url']) === 'undefined' || headers['variable_bitrix24_url'] === '') {
             return;
         }
-        callRinging(headers);
+        callRinging(headers, cache);
     })
     .on('esl::event::CHANNEL_BRIDGE::*', function(e) {
         let headers = headersProcess(e.headers);
         if (typeof(headers['variable_bitrix24_url']) === 'undefined' || headers['variable_bitrix24_url'] === '') {
             return;
         }
-        callAnswer(headers);
+        callAnswer(headers, cache);
     })
     .on('esl::event::CHANNEL_DESTROY::*', function(e) {
         let headers = headersProcess(e.headers);
@@ -30,13 +33,3 @@ freeswitch
     });
 
 log('VFusion daemon started');
-
-const userList = require('./lib/cache/getEmployeeList');
-
-userList('XXXXX', (err, res) => {
-    if (err) {
-        log(err);
-        return;
-    }
-    log(res);
-});
---
+++ fusion_daemon/app/lib/cache/b24callList.js
@@ -0,0 +1,77 @@
+const log = require('../../init/logger')(module),
+    request = require('urllib');
+
+let getB24callUuid = (callInfo, cache) => {
+
+    if (!callInfo['uuid']) {
+        return new Promise((resolve, reject) => {
+            reject("No UUID provided!");
+        });
+    }
+
+    let b24callUuid = cache.get('uuid_' + callInfo['uuid']);
+    if (b24callUuid) {
+        return b24callUuid;
+    }
+
+    b24callUuid = new Promise((resolve, reject) => {
+        if (!callInfo['userID']) {
+            reject("No UserID provided!");
+            return;
+        }
+        if (!callInfo['url']) {
+            reject("No Bitrix24 URL provided!");
+            return;
+        }
+        if (!callInfo['callerid']) {
+            reject("No callerID provided!");
+            return;
+        }
+
+        let requestURL = callInfo['url'] + "/telephony.externalcall.register.json?USER_ID=" + callInfo['userID'];
+        requestURL += "&PHONE_NUMBER=" + callInfo['callerid'];
+        requestURL += "&TYPE=2";
+        requestURL += "&CRM_CREATE=1";
+        requestURL += "&SHOW=0";
+
+        request.request(requestURL, (err, data, res) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+    
+            if (res.statusCode !== 200) {
+                reject("Server failed to answer with " + res.statusCode + " code");
+            }
+    
+            let registeredCall = data.toString();
+    
+            try {
+                registeredCall = JSON.parse(registeredCall);
+            } catch (e) {
+                reject("Answer from server is not JSON");
+                return;
+            }
+    
+            if (typeof registeredCall.result === 'undefined') {
+                reject("Missing result section in answer");
+                return;
+            }
+    
+            registeredCall = registeredCall.result;
+
+            if (!registeredCall['CALL_ID']) {
+                reject("Call ID is missing in answer");
+                return;
+            }
+
+            resolve(registeredCall['CALL_ID']);
+        });
+    });
+    
+    cache.put('uuid_' + callInfo['uuid'], b24callUuid, 10800); // Store for 3h
+    
+    return b24callUuid;
+}
+
+module.exports = getB24callUuid;
