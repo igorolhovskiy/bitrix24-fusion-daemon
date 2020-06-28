cfc89ad884bb0604d60977c2d9128d0595744f82
Debian <admin@ip-172-30-0-111.ec2.internal>
2020-06-28 08:19:49 +0000

    ...

--- fusion_daemon/app/lib/calls/hangup.js
+++ fusion_daemon/app/lib/calls/hangup.js
@@ -21,7 +21,7 @@ let hangup = (headers, cache) => {
                     || headers['variable_proto_specific_hangup_cause'] 
                     || headers['variable_sip_invite_failure_status']
                     || headers['variable_last_bridge_proto_specific_hangup_cause'];
-                    
+
                 if (!bitrix24Info['sip_code']) {
                     log("Cannot get correct hangup code, using 486");
                     log(headers);
@@ -52,55 +52,6 @@ let hangup = (headers, cache) => {
             .catch(err => {
                 log("Hangup: " + err);
             });
-    });
-
-    return;
-
-    let dialedUser = headers['Other-Leg-Destination-Number'];
-    let bitrix24Url = headers['variable_bitrix24_url'];
-
-    log("bridge Call was answered by " + dialedUser);
-
-    getB24EmployeeList(bitrix24Url, cache, (err, employeeList) => {
-
-        if (err) {
-            log("bridge Cannot get employeeList: " + err);
-            return;
-        }
-
-        if (typeof employeeList[dialedUser] === 'undefined') {
-            log("bridge User with extension " + dialedUser + " not found");
-            return;
-        }
-
-        let bitrix24Info = {
-            url: bitrix24Url,
-            userID: employeeList[dialedUser],
-            callUuid: headers['variable_call_uuid'] || headers['variable_uuid'],
-        }
-        
-        // Call function 500 ms after to make sure cache is populated
-        setTimeout(() => {
-            getB24CallInfo(bitrix24Info, cache)
-                .then((b24callInfo) => {
-
-                    bitrix24Info['b24uuid'] = b24callInfo['uuid'];
-                    if (b24callInfo['type'] === 2) { // Processing screens only for inbound calls
-
-                        log("bridge Hiding call screens...");
-
-                        hideCallScreen(bitrix24Info, cache, (err) => {
-                            if (err) {
-                                log("bridge" + err);
-                            }
-                        });
-                    }
-                }).catch((err) => {
-                    // If we can't get call UUID - do nothing. Really
-                    log("bridge " + err);
-                });
-        }, 500);
-
     });
 }
 
