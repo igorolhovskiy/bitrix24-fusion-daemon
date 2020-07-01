9303392f4bb491cb2ca02606f546fc4c8bd431dc
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-07-01 13:31:18 +0300

    Small fix on logger

--- fusion_daemon/app/init/logger.js
+++ fusion_daemon/app/init/logger.js
@@ -1,8 +1,8 @@
-const options = require('app/config/freeswitch');
+const options = require('app/config/log');
 
 module.exports = module => {
     return function() {
-        if (options.debug === 'true') {
+        if (options.debug) {
             let currentDate = new Date().toISOString();
             currentDate = currentDate.slice(0,10) + " " + currentDate.slice(11,19);
             let args = ["[" + currentDate +  "]" ,"[" + module.filename + "]"].concat([].slice.call(arguments));
