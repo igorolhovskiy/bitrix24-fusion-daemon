f9eb98d2f30d6174f7e0bda47dbcbdfcaae7ce50
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-07-08 16:02:59 +0300

    Moved rest (c2c) to other config file

--- fusion_daemon/app/config/bitrix.js
+++ fusion_daemon/app/config/bitrix.js
@@ -1,10 +1,5 @@
 module.exports = {
     url: (typeof process.env.BITRIX_URL === 'undefined') ? false : process.env.BITRIX_URL,
-    showCallNotifications : (typeof process.env.BITRIX_SHOWCALLNOTIFICATIONS === 'undefined') ? true : (process.env.BITRIX_SHOWCALLNOTIFICATIONS.toLowerCase() !== 'false'),
-    restEntryPoint: (typeof process.env.REST_ENTRYPOINT === 'undefined') ? false : process.env.REST_ENTRYPOINT,
-    restPort:  (typeof process.env.REST_PORT === 'undefined') ? 3000 : process.env.REST_PORT,
-    restToken: (typeof process.env.REST_TOKEN === 'undefined') ? process.env.REST_ENTRYPOINT : process.env.REST_TOKEN,
-    restRequestDomain: (typeof process.env.REST_REQUESTDOMAIN === 'undefined') ? "" : process.env.REST_REQUESTDOMAIN,
     createLocalContact: (typeof process.env.BITRIX_CREATELOCALCONTACT === 'undefined') ? false : (process.env.BITRIX_CREATELOCALCONTACT.toLowerCase() === 'true'),
     appendRecording: (typeof process.env.BITRIX_APPENDRECORDING === 'undefined') ? true : (process.env.BITRIX_APPENDRECORDING.toLowerCase() !== 'false'),
 };
