4c4659257b7dfb6213af95c2ebde585b0d7cf3b5
Debian <admin@ip-172-30-0-111.ec2.internal>
2020-07-24 12:22:57 +0000

    ...

--- fusion_daemon/app/lib/calls/create.js
+++ fusion_daemon/app/lib/calls/create.js
@@ -93,7 +93,7 @@ let create = (headers, cache) => {
 
                 createB24Call(bitrix24Info, cache)
                     .then(b24callInfo => {                    
-                        log("Registered generic call type  " + callType + " " + bitrix24Info['callUuid'] + " -> " + b24callInfo['uuid']);
+                        log("Registered generic call type " + callType + " " + bitrix24Info['callUuid'] + " -> " + b24callInfo['uuid']);
                     }).catch(err => {
                         // If we can't get call UUID - do nothing. Really
                         log("Registering generic call " + bitrix24Info['callUuid'] + " failed: " + err);
